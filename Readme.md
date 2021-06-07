3acdc5243e20b326d9ccbd2f3f2678939b8a768a
OTJxA <85507049+OTJxA@users.noreply.github.com>
2021-06-07 18:33:30 +0300

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,35 @@
+# سلام عليكم
+اول بروجيكت ميوزك اسويه و انشره 
+طبعا بروجيكت جدا بسيط
+سويته ب استعمال بكج
+**Distube**
+-------------------------------------------------
+1 - حط البرفكس ب ملف config
+
+2 - حط التوكن عند ال seceret environment
+
+3 - سوي رن و استمتع :D
+-------------------------------------------------
+الحقوق ل JxA  OT Team
+2021
+
+
+
+`اشوفك يل كوبي بستر تاخذ الحقوق موو`
+
+
+
+# Hi 
+this is my first music project 
+the project is soo simple
+i made it using **Distube** Package 
+-------------------------------------------------
+1 - put bot preifcc in config folder
+
+2 - put the token 
+
+3 - run the project and have fun :D
+-------------------------------------------------
+Copyright Reversed To JxA , OT Team :D
+
+`you know i can see copypasters right?`
---
+++ index.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js')
+const express = require("express");
+const app = express();
+app.listen(() => console.log("start ot || jxa"));
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
+const client = new Discord.Client();
+const fs = require('fs');
+const { Collection } = require('discord.js');
+const prefix = require('./config/bot.json');
+const distube = require('distube')
+const player = new distube(client)
+client.player = player
+
+fs.readdir('./events/', (err, files) => {
+  if (err) return console.log(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split('.')[0];
+    console.log(`Loading ${eventName}.js!`);
+    client.on(eventName, event.bind(null, client));
+  });
+});
+fs.readdir(`./commands/`, (error, files) => {
+  if (error) { return console.log("error i can not find commands"); };
+  files.forEach(file => {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+  })
+});
+
+player.on(`playSong`,(message , queue , song) => {
+var e = new Discord.MessageEmbed()
+.setTitle(`Now Playing`)
+.setDescription(`[${song.name}](${song.url}) `)
+.setColor(`GREEN`)
+message.channel.send(e)
+})
+player.on(`addSong`,(message , queue , song) => {
+var e = new Discord.MessageEmbed()
+.setTitle(`Added To Playlist`)
+.setDescription(`[${song.name}](${song.url}) `)
+.setColor(`RED`)
+message.channel.send(e)
+})
+player.on('initQueue', queue => {
+  queue.autoplay = false;
+})
+player.on(`finish`, (message , queue , song) => {
+message.channel.send(`voice channel is empty soo im leaving`)
+message.member.voice.channel.leave()
+}) 
+client.config = require('./config/bot.json');
+client.commands = new Discord.Collection();
+client.login(process.env.token)//bot token :D
+                                
